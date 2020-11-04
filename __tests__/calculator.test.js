56eec8cb0f34f83555c572125ea7316a096f11ca
Chris Ramer <chrisramer.97@gmail.com>
2020-11-04 11:23:30 -0800

    Remove comment

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,25 +1,3 @@
-/*
-import Rectangle from '../src/js/rectangle.js';
-
-describe('Rectangle', () => {
-
-	let rectangle;
-
-	beforeEach(() => {
-		rectangle = new Rectangle(3, 5);
-	});
-
-	test('should correctly create a rectangle object using two sides', () => {
-		expect(rectangle.side1).toEqual(3);
-		expect(rectangle.side2).toEqual(5);
-	});
-
-	test('should correctly create a rectangle object using two siddes', () => {
-		expect(rectangle.getArea()).toEqual(15);
-	});
-});
-*/
-
 import Birthday from '../src/js/calculator.js';
 
 describe('Calculator', () => {
