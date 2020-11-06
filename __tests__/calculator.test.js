ef82c0aad1d4e530e6d903f1dc10cb48783e836a
Chris Ramer <chrisramer.97@gmail.com>
2020-11-06 11:50:03 -0800

    Test: get planet years

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,14 +1,18 @@
-import Birthday from '../src/js/calculator.js';
+import Input from "../src/js/calculator.js";
 
-describe('Calculator', () => {
+describe("Calculator", () => {
 
-	let birthday;
+	let testInput;
 
 	beforeEach(() => {
-		birthday = new Birthday(new Date(1997, 7, 20));
+		testInput = new Input(new Date(1997, 7, 20), "Mercury");
 	});
 
-	test('should correctly get the inputted birthday as a formatted string', () => {
-		expect(birthday.getBirthday()).toEqual("8/20/1997");
+	test("should correctly get the inputted birthday as a formatted string", () => {
+		expect(testInput.getBirthday()).toEqual("8/20/1997");
+	});
+
+	test("should correctly get the inputted planet's years", () => {
+		expect(testInput.getPlanetYears()).toEqual(0.24);
 	});
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,13 +1,43 @@
 // AMAZING SITE https://www.exploratorium.edu/ronh/age/
 
-export default class Birthday {
-	constructor (birthday) {
+export default class Input {
+	constructor (birthday, planet) {
 		this.year = birthday.getFullYear();
 		this.month = birthday.getMonth() + 1;
 		this.day = birthday.getDate();
+		this.planet = planet;
 	}
+}
 
-	getBirthday() {
-		return this.month + "/" + this.day + "/" + this.year;
+Input.prototype.getBirthday = function() {
+	return this.month + "/" + this.day + "/" + this.year;
+}
+
+Input.prototype.getPlanet = function () {
+	return this.planet;
+}
+
+Input.prototype.getPlanetYears = function() {
+	switch (this.planet) {
+		case "Mercury":
+			return 0.24;
+		case "Venus":
+			return 0.62;
+		case "Earth":
+			return 1;
+		case "Mars":
+			return 1.88
+		case "Jupiter":
+			return 11.86
+		case "Saturn":
+			return 29;
+		case "Uranus":
+			return 84;
+		case "Neptune":
+			return 165;
+		case "Pluto":
+			return 248;
+		case "Planet 9":
+			return 15000;
 	}
 }
