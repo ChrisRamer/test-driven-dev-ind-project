a5a6c7cba5973cc5bee22c561774f96d72346fa1
Chris Ramer <chrisramer.97@gmail.com>
2020-11-06 11:50:53 -0800

    Test: Get planet name

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -12,6 +12,10 @@ describe("Calculator", () => {
 		expect(testInput.getBirthday()).toEqual("8/20/1997");
 	});
 
+	test("should correctly get the inputted planet name", () => {
+		expect(testInput.getPlanet()).toEqual("Mercury");
+	});
+
 	test("should correctly get the inputted planet's years", () => {
 		expect(testInput.getPlanetYears()).toEqual(0.24);
 	});
